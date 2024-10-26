efb65222b7e65cadf4fe31ecc1826df741e5e159
tduyng <tduyng.dev@gmail.com>
2024-10-26 09:30:45 +0200

    feat(3.0): support more colors

--- src/colors.ts
+++ src/colors.ts
@@ -1,12 +1,22 @@
 const formatter = (open: string, close: string) => (input: string) => open + input + close
 
 const createColors = () => ({
-    reset: formatter('\x1b[0m', '\x1b[0m'),
-    red: formatter('\x1b[31m', '\x1b[39m'),
-    yellow: formatter('\x1b[33m', '\x1b[39m'),
+    black: formatter('\x1b[30m', '\x1b[39m'),
     blue: formatter('\x1b[34m', '\x1b[39m'),
-    white: formatter('\x1b[37m', '\x1b[39m'),
+    brown: formatter('\x1b[38;5;94m', '\x1b[39m'),
+    cyan: formatter('\x1b[36m', '\x1b[39m'),
+    gray: formatter('\x1b[90m', '\x1b[39m'),
+    green: formatter('\x1b[32m', '\x1b[39m'),
     grey: formatter('\x1b[90m', '\x1b[39m'),
+    magenta: formatter('\x1b[35m', '\x1b[39m'),
+    orange: formatter('\x1b[38;5;208m', '\x1b[39m'),
+    pink: formatter('\x1b[38;5;205m', '\x1b[39m'),
+    purple: formatter('\x1b[38;5;93m', '\x1b[39m'),
+    rainbow: formatter('\x1b[31m', '\x1b[39m'),
+    red: formatter('\x1b[31m', '\x1b[39m'),
+    reset: formatter('\x1b[0m', '\x1b[0m'),
+    white: formatter('\x1b[37m', '\x1b[39m'),
+    yellow: formatter('\x1b[33m', '\x1b[39m'),
 })
 
 const colors = createColors()
